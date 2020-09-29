2c570581c3443d9e9bc517cc1768ca977be5a5ff
Khairan <alphakhairan@gmail.com>
2020-09-29 20:04:15 +0300

    Доделал оставшиеся 3 задания по 2 уроку

---
+++ Lesson2/HwN5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HwN1.Program;
+
+//Глотов Андрей
+// №5
+//а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
+//б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+
+namespace HwN5
+{        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double underweight = 18.50, overweight = 25, kg;
+
+            Console.WriteLine("Рассчет индекса массы тела (ИМТ).\nВес указывается в килограммах (80), а рост в метрах (1,80)");
+            double m = Convert.ToDouble(ParamRequest("Вес"));
+            double h = Convert.ToDouble(ParamRequest("Рост"));
+
+            double imt = m / Math.Pow(h, 2);
+
+            Console.WriteLine($"\nВаш ИМТ = {imt:f2}");
+
+            if (imt < underweight)
+            {
+                kg = (underweight - imt) * Math.Pow(h, 2);
+                Console.WriteLine($"Вам нужно набрать {kg:f2} кг");
+            }
+            else if (imt > overweight)
+            {
+                kg = (imt - overweight) * Math.Pow(h, 2);                
+                Console.WriteLine($"Вам нужно скинуть {kg:f2} кг");
+            }
+            else Console.WriteLine("Ваш вес в норме");
+
+            Signature();
+        }
+    }
+}
---
+++ Lesson2/HwN6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HwN1.Program;
+
+//Глотов Андрей
+// №6
+//*Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+//«Хорошим» называется число, которое делится на сумму своих цифр.
+//Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+
+namespace HwN6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Подсчет количества 'хороших' чисел в диапазоне от 1 до 1 000 000 000");
+
+            int maxValue = 1000000;
+            int counter = 0;
+
+            DateTime start = DateTime.Now;
+
+            for (int i = 1; i <= maxValue; i++)
+            {
+                int temp = i;
+                int sum = 0;
+                while (temp > 0)
+                {
+                    sum = sum + (temp % 10);
+                    temp = temp/10;
+                }
+                if (i % sum == 0) counter++;
+            }
+                        
+            Console.WriteLine($"Хороших чисел в числе {maxValue:### ### ###} = {counter}");
+            Console.WriteLine($"Затрачено времени на подсчет {DateTime.Now - start}");
+            Signature();
+
+        }
+    }
+}
