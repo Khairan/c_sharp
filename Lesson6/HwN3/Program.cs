c56835050dc6b76b0a20aba9cfcc1eb9a32a37c5
Khairan <alphakhairan@gmail.com>
2020-10-13 17:06:25 +0300

    Подправил 3 задание

--- Lesson6/HwN3/Program.cs
+++ Lesson6/HwN3/Program.cs
@@ -58,6 +58,8 @@ namespace HwN3
         {
             int bakalavr = 0;
             int magistr = 0;
+            int[] ageArray = new int[100];
+            
             List<Student> list = new List<Student>(); // Создаем список студентов
             DateTime dt = DateTime.Now;
             StreamReader sr = new StreamReader("students_1.csv", Encoding.GetEncoding(1251));
@@ -67,9 +69,11 @@ namespace HwN3
                 {
                     string[] s = sr.ReadLine().Split(';');
                     // Добавляем в список новый экземпляр класса Student
-                    list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                    int age = int.Parse(s[5]);
+                    list.Add(new Student(s[0], s[1], s[2], s[3], s[4], age, int.Parse(s[6]), int.Parse(s[7]), s[8]));
                     // Одновременно подсчитываем количество бакалавров и магистров
                     if (int.Parse(s[6]) < 5) bakalavr++; else magistr++;
+                    ageArray[age]++; //заполнение частотного массива
                 }
                 catch (Exception e)
                 {
@@ -84,9 +88,15 @@ namespace HwN3
             list.Sort(SortAge);
             list.Sort(SortCourse);            
             Console.WriteLine("Всего студентов: " + list.Count);
-            Console.WriteLine("Магистров(1-4 курс): {0}", magistr);
-            Console.WriteLine("Бакалавров(5-6 курс): {0}\n", bakalavr);
+            Console.WriteLine("Бакалавров(1-4 курс): {0}", bakalavr);
+            Console.WriteLine("Магистров(5-6 курс): {0}\n", magistr);
             foreach (var v in list) Console.WriteLine($"{v.firstName} {v.lastName} возраст {v.age} курс {v.course}");
+            int youngStudents = 0;
+            for (int i = 18; i <= 20; i++)
+            {
+                youngStudents += ageArray[i];
+            }
+            Console.WriteLine($"\nСтуденты в возрасте от 18 до 20 лет: {youngStudents}");
             Console.WriteLine($"\n{DateTime.Now - dt}");
             Console.ReadKey();
         }
