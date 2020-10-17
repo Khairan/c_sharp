00f88f5d046a02651495fb191650642a144b7919
Khairan <alphakhairan@gmail.com>
2020-10-17 12:54:24 +0300

    Сделал урок №7

---
+++ Lesson7/HwN1/FormMultiplier.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Глотов Андрей
+//задание №1
+//а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+//б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок. Игрок должен получить это число за минимальное количество ходов.
+//в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте обобщенный класс Stack.
+//Вся логика игры должна быть реализована в классе с удвоителем.
+
+namespace HwN1
+{
+    public partial class FormMultiplier : System.Windows.Forms.Form
+    {
+        public int guessnumber;
+
+        public FormMultiplier()
+        {
+            InitializeComponent();
+            btnCommand1.Text = "+1";
+            btnCommand2.Text = "x2";
+            btnReset.Text = "Сброс";
+            lblNumber.Text = "0";
+            this.Text = "Удвоитель";
+            commandNumber.Text = "0";
+            guessNumber.Text = "Число 0";
+                        
+        }
+
+        private void IncreaseCommandNumber()
+        {
+            commandNumber.Text = (int.Parse(commandNumber.Text) + 1).ToString();
+            if (guessnumber == int.Parse(lblNumber.Text)) MessageBox.Show($"Вы достигли числа за {commandNumber.Text} ходов");
+        }
+
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            IncreaseCommandNumber();
+        }      
+
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            IncreaseCommandNumber();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "1";
+            IncreaseCommandNumber();
+        }
+
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "0";
+            commandNumber.Text = "0";
+            var rnd = new Random();
+            guessnumber = rnd.Next(1, 1000);
+            MessageBox.Show($"Получите число {guessnumber} за минимальное количество ходов");
+            guessNumber.Text = "Число " + guessnumber.ToString();
+        }
+    }
+}
---
+++ Lesson7/HwN2/FormGuessNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Глотов Андрей
+//задание №2
+//Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток. Компьютер говорит, больше или меньше загаданное число введенного.  
+//a) Для ввода данных от человека используется элемент TextBox;
+//б) **Реализовать отдельную форму c TextBox для ввода числа.
+
+namespace HwN2
+{
+    public partial class FormGuessNumber : Form
+    {
+        public int guessedNumber;
+
+        public FormGuessNumber()
+        {
+            InitializeComponent();
+            this.Text = "Угадай число";
+            var rnd = new Random();
+            guessedNumber = rnd.Next(1, 100);
+        }
+
+        private void userNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                int number = int.Parse(userNumber.Text);
+                if (number == guessedNumber) { MessageBox.Show("Поздравляю, вы отгадали!"); Close(); }
+                else if (number > guessedNumber) MessageBox.Show("Слишком большое число");
+                else MessageBox.Show("Слишком маленькое число");
+            }
+        }
+               
+    }
+}
